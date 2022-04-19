0ed580af0506adfac126c50e52a827963c268f7b
Irina Yegorov <yeg.irinaca@gmail.com>
2022-04-19 14:06:51 -0700

    commit

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,7 +1,6 @@
 import com.example.Feline;
 import com.example.Lion;
 import org.junit.Assert;
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
